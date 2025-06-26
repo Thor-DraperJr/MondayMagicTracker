cad72c32a19fe1f66c9f1483282b8c268ac0488b
Thor Draper <admin@MngEnvMCAP392206.onmicrosoft.com>
2025-06-25 22:17:40 -0400

    Fix React build and enable Swagger in production

--- Program.cs
+++ Program.cs
@@ -124,8 +124,6 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
-    app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Monday Magic Tracker API v1"));
     app.UseCors("AllowReactApp");
 }
 else
@@ -133,6 +131,14 @@ else
     app.UseExceptionHandler("/Error");
     app.UseHsts();
 }
+
+// Enable Swagger in all environments for testing
+app.UseSwagger();
+app.UseSwaggerUI(c => 
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Monday Magic Tracker API v1");
+    c.RoutePrefix = "swagger";
+});
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
